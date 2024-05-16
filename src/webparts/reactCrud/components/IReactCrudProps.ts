10cd28996e97735e36ac3632744f8958fc8b45a7
rezmes <rezmes@gmail.com>
2024-05-16 15:56:14 +0330

    First-Commit

---
+++ src/webparts/reactCrud/components/IReactCrudProps.ts
@@ -0,0 +1,3 @@
+export interface IReactCrudProps {
+  description: string;
+}
---
+++ src/webparts/reactCrud/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReactCrudWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReactCrudWebPartStrings' {
+  const strings: IReactCrudWebPartStrings;
+  export = strings;
+}

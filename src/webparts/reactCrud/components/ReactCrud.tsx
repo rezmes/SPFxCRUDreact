09c3a9fbf0314bf764721c2d9e5e5776dc2662fe
rezmes <rezmes@gmail.com>
2024-05-18 12:34:02 +0330

    Also 2

--- src/webparts/reactCrud/components/ReactCrud.tsx
+++ src/webparts/reactCrud/components/ReactCrud.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import styles from './ReactCrud.module.scss';
 import { IReactCrudProps } from './IReactCrudProps';
-import { escape } from '@microsoft/sp-lodash-subset';
 import {
   TextField,
   Dropdown,
@@ -10,120 +9,73 @@ import {
   PrimaryButton,
   DetailsList,
   CheckboxVisibility,
-  SelectionMode,
   DetailsListLayoutMode
-} from "office-ui-fabric-react";
+} from 'office-ui-fabric-react';
 import { LIST_COLUMNS } from '../shared/constants';
 
+const AddICon: IIconProps = { iconName: 'Add' };
+const ReadICon: IIconProps = { iconName: 'BulletedListText' };
+const SaveICon: IIconProps = { iconName: 'Save' };
+const DeleteICon: IIconProps = { iconName: 'Delete' };
 
-const stackTokens = {childrenGap:50}
-const AddICon: IIconProps = {iconName: "Add" }
-const ReadICon: IIconProps = {iconName: "BulletedListText" }
-const SaveICon: IIconProps = {iconName: "Save" }
-const DeleteICon: IIconProps = {iconName: "Delete" }
+const ddlBatchOptions: IDropdownOption[] = [
+  { key: 'Batch 1', text: 'Batch 1' },
+  { key: 'Batch 2', text: 'Batch 2' },
+  { key: 'Batch 3', text: 'Batch 3' }
+];
 
-const stackStyles: Partial<IstackStyles> = {root:{width:650}}
+const ddlLevelOFKnowledgeOptions: IDropdownOption[] = [
+  { key: 'Beginner', text: 'Beginner' },
+  { key: 'Intermediate', text: 'Intermediate' },
+  { key: 'Expert', text: 'Expert' }
+];
 
-const dropdownStyles: Partial<IDropdownStyles> = {dropdown:{width:300}}
-
-const ddlBatchOptions : IDropdownOption[]=[
-  {key: "Batch 1", text:"Batch 1"},
-  {key: "Batch 2", text:"Batch 2"},
-  {key: "Batch 3", text:"Batch 3"}
-]
-
-const ddlLevelOFKnowledgeOptions : IDropdownOption[]=[
-  {key: "Beginner", text:"Beginner"},
-  {key: "Intermediate", text:"Intermediate"},
-  {key: "Expert", text:"Expert"}
-]
-
-const columnProps : Partial<IStackProps>= {
-  tokens : {childrenGap:15},
-  styles : {root: {width: 300}}
-}
-
-
-export default class ReactCrud extends React.Component < IReactCrudProps, {} > {
+export default class ReactCrud extends React.Component<IReactCrudProps, {}> {
   public render(): React.ReactElement<IReactCrudProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
-    return(
+    return (
       <div>
-        <Stack horizontal tokens= {stackTokens} styles={stackStyles}>
-          <Stack {...columnProps}>
-            <TextField
-            label= "Username" 
-            placeholder='Please enter username'
-            />
-            <TextField
-            label= "Email" 
-            placeholder='Please enter your email address'
+        <div className={styles.rootStack}>
+          <div className={styles.columnStack}>
+            <TextField label="Username" placeholder="Please enter username" />
+            <TextField label="Email" placeholder="Please enter your email address" />
+            <Dropdown
+              label="Batch Number"
+              options={ddlBatchOptions}
+              className={styles.dropdownCustom}
             />
             <Dropdown
-            label= "BatchNumber" 
-            options={ddlBatchOptions}
-            styles = {dropdownStyles}
-            />            
+              label="Select Level Of Knowledge"
+              options={ddlLevelOFKnowledgeOptions}
+              className={styles.dropdownCustom}
+            />
+          </div>
+        </div>
 
-            <Dropdown
-            label= "Select Level Of Knowledge" 
-            options={ddlLevelOFKnowledgeOptions}
-            styles = {dropdownStyles}
-            />            
-
-          </Stack>
-        </Stack>
-
-<hr />
-<Stack horizontal tokens={stackTokens}>
-  <PrimaryButton 
-  text = 'Create'
-  iconProps={AddICon}
-   />
-  <PrimaryButton 
-  text = 'Read'
-  iconProps={ReadICon}
-   />
-  <PrimaryButton 
-  text = 'Update'
-  iconProps={SaveICon}
-   />   
-  <PrimaryButton 
-  text = 'Delete'
-  iconProps={DeleteICon}
-   />
-  </Stack>
-  <div id='divStatus'></div>
-  <hr />
-  <DetailsList
-  items={[{Id: '0',Title:'Dummy Title', Email: 'dummy@abc.com', Batch: 'Batch 1', 'LevelOfKnowledge':'LevelOfKnowledge'}]}
-  columns={LIST_COLUMNS}
-  setKey='Id'
-  checkboxVisibility={CheckboxVisibility.onHover}
-  layoutMode={DetailsListLayoutMode}
-  />
+        <hr />
+        <div className={styles.primaryButtonGroup}>
+          <PrimaryButton text="Create" iconProps={AddICon} />
+          <PrimaryButton text="Read" iconProps={ReadICon} />
+          <PrimaryButton text="Update" iconProps={SaveICon} />
+          <PrimaryButton text="Delete" iconProps={DeleteICon} />
+        </div>
+        <div id="divStatus"></div>
+        <hr />
+        <DetailsList
+          items={[
+            {
+              Id: '0',
+              Title: 'Dummy Title',
+              Email: 'dummy@abc.com',
+              Batch: 'Batch 1',
+              LevelOfKnowledge: 'LevelOfKnowledge'
+            }
+          ]}
+          columns={LIST_COLUMNS}
+          setKey="Id"
+          checkboxVisibility={CheckboxVisibility.onHover}
+          layoutMode={DetailsListLayoutMode.fixedColumns}
+        />
       </div>
-
-  //     <div className = { styles.reactCrud } >
-  // <div className={styles.container}>
-  //   <div className={styles.row}>
-  //     <div className={styles.column}>
-  //       <span className={styles.title}>Welcome to SharePoint!</span>
-  //       <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
-  //       <p className={styles.description}>{escape(this.props.description)}</p>
-  //       <a href='https://aka.ms/spfx' className={styles.button}>
-  //         <span className={styles.label}>Learn more</span>
-  //       </a>
-  //     </div>
-  //   </div>
-  // </div>
-  //     </div >
     );
   }
 }
